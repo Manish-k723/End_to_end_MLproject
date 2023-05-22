0a92a417545eeb9a4801e8e7945b8fe0aea3866b
Manish-k723 <manishkumar_me20b17_56@dtu.ac.in>
2023-05-22 15:52:06 +0530

    Hyperparameters tuned

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 # from src.logger import logging
 
 def save_obj(file_path, obj):
@@ -18,12 +19,18 @@ def save_obj(file_path, obj):
             dill.dump(obj, f)
     except Exception as e:
         raise CustomException(e,sys)
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = list(param.values())[i]
+
+            grid_search = GridSearchCV(model, para, cv=3, )
+            grid_search.fit(X_train,y_train)
+
+            model.set_params(**grid_search.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
@@ -32,9 +39,10 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
             train_score = r2_score(y_train_pred,y_train)
             test_score = r2_score(y_test_pred,y_test)
 
+            print(list(models.keys())[i],train_score, test_score)
             report[list(models.keys())[i]] = [train_score,test_score]
 
-            return report 
+        return report 
     except Exception as e:
         raise CustomException(e,sys)
 
